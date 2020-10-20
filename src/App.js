320896ffa0c99eb478589cdbddc5f93120a39f79
areddin409 <areddin409@gmail.com>
2020-10-19 22:05:14 -0500

    updated menu-items to nagivate withRouther HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = (props) => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
-     </div>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
+    </div>
   );
 }
 
